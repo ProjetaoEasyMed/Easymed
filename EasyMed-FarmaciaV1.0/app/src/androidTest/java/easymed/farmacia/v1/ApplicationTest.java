928f975b4286410f9acce6eb5cbc46d4ab82e35a
rubemmoura <rfmj2@cin.ufpe.br>
2016-06-26 15:30:14 -0300

    EasyMedV1
    Criando projeto inicial.

---
+++ EasyMed-FarmaciaV1.0/app/src/androidTest/java/easymed/farmacia/v1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package easymed.farmacia.v1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
