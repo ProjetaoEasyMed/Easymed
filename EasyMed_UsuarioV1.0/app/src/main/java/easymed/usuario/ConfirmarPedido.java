936169f270c9339d0f097efce4713df7de84ea57
rubemmoura <rfmj2@cin.ufpe.br>
2016-06-26 21:50:22 -0300

    Criando EasyMed lado Usuário
    Criando EasyMed lado Usuário

---
+++ EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/ConfirmarPedido.java
@@ -0,0 +1,13 @@
+package easymed.usuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmarPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_pedido);
+    }
+}
