8e6f6884e3e59e622961338680eaff71909bcf9c
rubemmoura <rfmj2@cin.ufpe.br>
2016-07-04 21:09:46 -0300

    Tela de Ok, com botoes Aguarde e DefinirPadrao criadas conformeimagem do quadro.

--- EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/PedidoRealizado.java
+++ EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/PedidoRealizado.java
@@ -1,13 +1,47 @@
 package easymed.usuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PedidoRealizado extends AppCompatActivity {
+
+    private Button definir_padrao;
+    private Button home_screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido_realizado);
+
+        definir_padrao = (Button) findViewById(R.id.definir_padrao);
+        definir_padrao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(PedidoRealizado.this, MainActivity.class);
+//                enviar para o botao o padrao definido
+
+//                it.putExtra("fbJsonObj", jsonObj.toString());
+//                it.putExtra("jsonObjDados",jsonObjDados.toString());
+                startActivity(it);
+                finish();
+            }
+        });
+
+        home_screen = (Button) findViewById(R.id.home_screen);
+        home_screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(PedidoRealizado.this, MainActivity.class);
+//                it.putExtra("fbJsonObj", jsonObj.toString());
+//                it.putExtra("jsonObjDados",jsonObjDados.toString());
+                startActivity(it);
+                finish();
+            }
+        });
+
+
     }
 }
