12d700ce2b18dd6ae609829811760955effb673b
Wilson Limeira <wilson.limeira@gmail.com>
2016-07-10 17:50:24 -0300

    Implementar botão de pesquisa na tela ScrollingActivity.
    - Botão de Pesquisa funciona
    - Campo de busca funciona a mostra msg caso não tenha nada
    - Apaga o texto anterior na tela e mantem o menu
    - FALTA IMPLEMENTAR FILTRO

--- EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/ScrollingActivity.java
+++ EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/ScrollingActivity.java
@@ -6,8 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Space;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.Vector;
+
+import easymed.usuario.produtos.ProdutoInfo;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private TableLayout listMedicines;
+    private EditText barraPesquisa;
+
+    private Vector<ProdutoInfo> medicamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +35,66 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String palavraBuscada = barraPesquisa.getText().toString();
+
+                if(palavraBuscada.length() > 0)
+                    filtrarLista(palavraBuscada);
+                else
+                {
+                    Snackbar.make(view, "Campo de busca vazio", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        /* O MONSTRO SAIU DA JAULA!!! */
+        listMedicines = (TableLayout) findViewById(R.id.listMedicines);
+        barraPesquisa = (EditText) findViewById(R.id.barraPesquisa);
+
+        medicamentos = new Vector<ProdutoInfo>();
+
+        //Povoamento de Exemplo
+        medicamentos.add(new ProdutoInfo(1,"Viagra", "Azulzinho", 15.90, "caixa", 10));
+        medicamentos.add(new ProdutoInfo(2,"Seringas", "BD", 3.50, "pacote", 20));
+        medicamentos.add(new ProdutoInfo(3,"Insulina", "Apidra", 25.99, "refil", 1));
+        medicamentos.add(new ProdutoInfo(4,"Curativos", "Band-Aid", 2.00, "caixa", 10));
+        medicamentos.add(new ProdutoInfo(5,"Mussum Ipsum", "Mussum", 10000000.00, "ipsum", 10000));
+        medicamentos.add(new ProdutoInfo(6,"Licor de Cacau", "Xavier", 5.00, "ml", 50));
+        //Fim do Povoamento
+
+        for(int i = 0; i < medicamentos.size(); i++)
+        {
+            TableRow linha = new TableRow(this);
+
+            TextView nome = new TextView(this);
+            nome.setTextAppearance(this, android.R.style.TextAppearance_Medium);
+            TextView marca = new TextView(this);
+            marca.setTextAppearance(this, android.R.style.TextAppearance_Medium);
+            TextView preco = new TextView(this);
+            preco.setTextAppearance(this, android.R.style.TextAppearance_Medium);
+
+            nome.setText(medicamentos.get(i).getNome());
+            marca.setText(medicamentos.get(i).getMarca());
+            String p = "RS " + String.format("%.2f", medicamentos.get(i).getPreco());
+            preco.setText(p);
+
+            linha.addView(nome);
+            linha.addView(new Space(this));
+            linha.addView(marca);
+            linha.addView(new Space(this));
+            linha.addView(preco);
+
+            listMedicines.addView(linha, new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
     }
+
+    private void filtrarLista(String word)
+    {
+        listMedicines.removeViews(1, medicamentos.size()); //FODAAAAAO: Remove as views, mas deixa o Menu
+
+
+    }
+
 }
