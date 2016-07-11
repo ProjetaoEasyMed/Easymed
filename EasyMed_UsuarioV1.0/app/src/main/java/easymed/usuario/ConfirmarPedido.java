d9994f8914590dd5f988f67285417e215a0542f2
rubemmoura <rfmj2@cin.ufpe.br>
2016-07-10 23:36:14 -0300

    POST
    Enviando lista de pedido num json, atraves de um post, ao clicar no
    botao confirmar.

--- EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/ConfirmarPedido.java
+++ EasyMed_UsuarioV1.0/app/src/main/java/easymed/usuario/ConfirmarPedido.java
@@ -1,21 +1,37 @@
 package easymed.usuario;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ConfirmarPedido extends AppCompatActivity {
 
     private Button back_button;
     private Button confirm_button;
 
+    private RequestQueue requestQueue;
+
+    JSONObject jsonObj = new JSONObject();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_pedido);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         back_button = (Button) findViewById(R.id.back_button);
         back_button.setOnClickListener(new View.OnClickListener() {
@@ -33,16 +49,38 @@ public class ConfirmarPedido extends AppCompatActivity {
         confirm_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                try {
+                    jsonObj.put("nome","fralda pampers");
+                    jsonObj.put("quantidade","2");
+                    jsonObj.put("tipo","produto");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                final JsonObjectRequest jsonObjectRequest2 = new JsonObjectRequest(Request.Method.POST,
+                        "https://projetao-easymed.herokuapp.com/atualizaLista",
+                        jsonObj,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                    Toast.makeText(ConfirmarPedido.this, "Enviando lista para o servidor   /atualizaLista", Toast.LENGTH_LONG).show();
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                            }
+                        }
+                );
+                requestQueue.add(jsonObjectRequest2);
+
                 Intent it = new Intent(ConfirmarPedido.this, Aguarde.class);
-//                it.putExtra("fbJsonObj", jsonObj.toString());
-//                it.putExtra("jsonObjDados",jsonObjDados.toString());
                 startActivity(it);
                 finish();
             }
         });
-
-
-
     }
 
     @Override
